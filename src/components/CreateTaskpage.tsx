8e85e417e1a56e4fef5f58c8c2de9f2870afa21b
MegharajKoli <megharajkoli104@gmail.com>
2026-02-26 11:59:10 +0530

    Separated reusable component taskForm

---
+++ src/components/CreateTaskpage.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import { TaskForm } from './templates/TaskForm';
+import { createTask } from '../store/taskSlice';
+import { useAppDispatch, useAppSelector } from '../store';
+import type { CreateTaskDTO } from '../types';
+
+export const CreateTaskPage: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { submitting } = useAppSelector(state => state.tasks);
+
+  const handleCreate = async (data: CreateTaskDTO) => {
+    try {
+      await dispatch(createTask(data)).unwrap();
+      navigate('/tasks'); 
+    } catch (err) {
+      console.error("Create failed", err);
+    }
+  };
+
+  return (
+    <TaskForm 
+      onSubmit={handleCreate} 
+      onCancel={() => navigate('/tasks')} 
+      submitting={submitting} 
+    />
+  );
+};
